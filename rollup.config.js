f657094c9514f647ac993b0e7e8d7e72d77eb0ba
luankefei <luankefei@gmail.com>
2020-05-01 18:49:00 +0800

    chore: rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,35 @@
+import fs from "fs";
 import { rollup } from "rollup";
 import typescript from "@rollup/plugin-typescript";
-import sourcemaps from "rollup-plugin-sourcemaps";
+// import sourcemaps from "rollup-plugin-sourcemaps";
 
 // rollup.config.js
-export default rollup({
+const config = {
   input: "src/main.ts",
   plugins: [
     typescript({
       exclude: "node_modules/**",
       module: "ESNext",
     }),
-    sourcemaps(),
+    // sourcemaps(),
   ],
   output: {
-    file: "bundle.js",
+    name: "$",
+    file: "dist/index.js",
     format: "iife",
   },
-});
+};
+
+// export default
+export default rollup(config)
+  .then((bundle) => bundle.generate(config.output))
+  .then(({ output: [{ code }] }) => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(config.output.file, code, (err) => {
+        if (err) return reject(err);
+
+        console.log("TODO: without gzipped");
+        resolve();
+      });
+    });
+  });
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+function test() {
+  console.log("hello world");
+}
+
+console.log("oooojbk");
+
+export default test;
