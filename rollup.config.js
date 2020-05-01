629eee0ca6bfed2418f4a51edba1076ef7936f05
luankefei <luankefei@gmail.com>
2020-05-01 16:42:34 +0800

    refactor: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,19 @@
+import { rollup } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import sourcemaps from "rollup-plugin-sourcemaps";
+
 // rollup.config.js
-export default {
-  input: "src/main.js",
+export default rollup({
+  input: "src/main.ts",
+  plugins: [
+    typescript({
+      exclude: "node_modules/**",
+      module: "ESNext",
+    }),
+    sourcemaps(),
+  ],
   output: {
     file: "bundle.js",
-    format: "cjs",
+    format: "iife",
   },
-};
+});
