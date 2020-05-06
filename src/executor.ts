85a9031154b0348da683634233ccea9f2a32bf67
luankefei <luankefei@gmail.com>
2020-05-06 10:18:52 +0800

    refactor: replace screen.width -> context.canvas.width

--- src/executor.ts
+++ src/executor.ts
@@ -4,7 +4,7 @@ const runInPlace = {
   lastAdvance: 0,
   PAGEFLIP_INTERVAL: 1000,
 
-  excute: function (sprite: Sprite, _: CanvasRenderingContext2D, now: number) {
+  execute(sprite: Sprite, _: CanvasRenderingContext2D, now: number) {
     if (now - this.lastAdvance > this.PAGEFLIP_INTERVAL) {
       sprite.painter.advance();
       this.lastAdvance = now;
@@ -15,17 +15,12 @@ const runInPlace = {
 const moveLeftToRight = {
   lastMove: 0,
 
-  execute: function (
-    sprite: Sprite,
-    _: CanvasRenderingContext2D,
-    time: number
-  ) {
+  execute(sprite: Sprite, context: CanvasRenderingContext2D, time: number) {
     if (this.lastMove !== 0) {
       sprite.left -= sprite.velocityX * ((time - this.lastMove) * 1000);
 
       if (sprite.left < 0) {
-        // TODO: 使用screen替代了canvas.width
-        sprite.left = screen.width;
+        sprite.left = context.canvas.width;
       }
     }
     this.lastMove = time;
